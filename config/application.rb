e066368fe3fbbf02e8630370e7c72bd129bfadc4
Serg3 <jaguar.sergey@gmail.com>
2018-07-08 22:03:21 +0300

    setup rspec and factories

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,15 @@ module MyCarExpenses
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_spec: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false,
+                       controller_spec: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
