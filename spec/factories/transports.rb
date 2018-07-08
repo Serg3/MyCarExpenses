e22e0e16c61c34d3a5fab2e86bcf639a09751e22
Serg3 <jaguar.sergey@gmail.com>
2018-07-08 22:05:04 +0300

    create transport's factory

---
+++ spec/factories/transports.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  sequence :name do |n|
+    "TestTransport#{n}"
+  end
+
+  factory :transport do
+    name
+    mark 'TestMark'
+    model 'TestModel'
+    edition_year 'TestYear'
+  end
+
+  factory :invalid_transport, class: 'Transport' do
+    name nil
+    mark nil
+    model nil
+    edition_year nil
+  end
+end
