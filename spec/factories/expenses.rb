11fbad24a4fa74eb5ee08b703f18911b449ef37e
Serg3 <jaguar.sergey@gmail.com>
2018-07-09 23:23:55 +0300

    edit expense's factory

--- spec/factories/expenses.rb
+++ spec/factories/expenses.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
+  sequence :description do |n|
+    "TestDescription#{n}"
+  end
+
   factory :expense do
-    
+    date Time.now
+    mileage 123456
+    kind 'TestKind'
+    description
+    amount 49.99
+  end
+
+  factory :invalid_expense, class: 'Expense' do
+    date nil
+    mileage nil
+    kind nil
+    description nil
+    amount nil
   end
 end
